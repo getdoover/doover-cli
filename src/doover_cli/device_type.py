16be9a78d7787c511ca83c5d44b6d41a069c17e5
mathsman5133 <mathsman5132@gmail.com>
2025-08-27 21:54:41 +1000

    add device type standalone script publisher

--- src/doover_cli/device_type.py
+++ src/doover_cli/device_type.py
@@ -12,7 +12,7 @@ app = typer.Typer(no_args_is_help=True)
 
 
 @app.command()
-def upload_installer(
+def upload_installer_tar(
     ctx: typer.Context,
     device_type_id: Annotated[int, typer.Argument(help="Device type ID to upload to")],
     installer_fp: Annotated[
@@ -26,7 +26,7 @@ def upload_installer(
         ),
     ] = None,
 ):
-    """Upload an installer to the Doover 2.0 Control Plane API."""
+    """Compress and upload an installer to the Doover 2.0 Control Plane API."""
     fp = Path(f"/tmp/{uuid.uuid4()}")
     shutil.make_archive(
         base_name=str(fp), format="gztar", root_dir=installer_fp.resolve()
@@ -34,6 +34,31 @@ def upload_installer(
     resp = requests.patch(
         f"{BASE_URL}/devices/types/{device_type_id}/",
         files={"installer": open(f"{fp}.tar.gz", "rb")},
+        headers={"Authorization": f"Bearer {token}"},
+    )
+    resp.raise_for_status()
+    print("Successfully uploaded installer.")
+
+
+@app.command()
+def upload_installer(
+    ctx: typer.Context,
+    device_type_id: Annotated[int, typer.Argument(help="Device type ID to upload to")],
+    installer_fp: Annotated[
+        Path, typer.Argument(help="Path to the installer script.")
+    ] = Path(),
+    token: Annotated[
+        str,
+        typer.Option(
+            "--token",
+            help="Authentication token.",
+        ),
+    ] = None,
+):
+    """Upload an installer to the Doover 2.0 Control Plane API."""
+    resp = requests.patch(
+        f"{BASE_URL}/devices/types/{device_type_id}/",
+        files={"installer": installer_fp.read_text()},
         headers={"Authorization": f"Bearer {token}"},
     )
     resp.raise_for_status()
